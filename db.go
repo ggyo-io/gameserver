c846e9107ec1ff607db14545bbe24d462f00a391
Yuval Rimar <yuval.rimar@servicenow.com>
2018-04-13 09:54:35 +0300

    orm

---
+++ db.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"time"
+)
+
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "postgres"
+	DB_NAME     = "test"
+)
+
+func main() {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", dbinfo)
+	checkErr(err)
+	defer db.Close()
+
+	fmt.Println("# Inserting values")
+
+	var lastInsertId int
+	err = db.QueryRow("INSERT INTO user(username,created) VALUES($1,$2) returning uid;", "astaxie", "2012-12-09").Scan(&lastInsertId)
+	checkErr(err)
+	fmt.Println("last inserted id =", lastInsertId)
+
+	fmt.Println("# Updating")
+	stmt, err := db.Prepare("update user set username=$1 where uid=$2")
+	checkErr(err)
+
+	res, err := stmt.Exec("astaxieupdate", lastInsertId)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect, "rows changed")
+
+	fmt.Println("# Querying")
+	rows, err := db.Query("SELECT * FROM user")
+	checkErr(err)
+
+	for rows.Next() {
+		var uid int
+		var username string
+		var department string
+		var created time.Time
+		err = rows.Scan(&uid, &username, &department, &created)
+		checkErr(err)
+		fmt.Println("uid | username | department | created ")
+		fmt.Printf("%3v | %8v | %6v | %6v\n", uid, username, department, created)
+	}
+
+	fmt.Println("# Deleting")
+	stmt, err = db.Prepare("delete from user where uid=$1")
+	checkErr(err)
+
+	res, err = stmt.Exec(lastInsertId)
+	checkErr(err)
+
+	affect, err = res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect, "rows changed")
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ orm.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+"github.com/jinzhu/gorm"
+_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type Product struct {
+	gorm.Model
+	Code string
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open("mysql", "root:@/test?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		println("ERRRR connecting")
+		panic(err)
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "L1212", Price: 1000})
+
+	// Read
+	var product Product
+	db.First(&product, 1) // find product with id 1
+	db.First(&product, "code = ?", "L1212") // find product with code l1212
+
+	// Update - update product's price to 2000
+	db.Model(&product).Update("Price", 2000)
+
+	// Delete - delete product
+	//db.Delete(&product)
+}
