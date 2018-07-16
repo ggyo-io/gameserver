0b9274798c66b099c5e3efe03f318ff176b4f11c
Yuval Rimar <yuval.rimar@servicenow.com>
2018-07-16 13:57:00 -0700

    add response code logging

--- logger.go
+++ logger.go
@@ -6,14 +6,26 @@ import (
 	"time"
 )
 
+// Make a wrapper for ResonseWriter to be able to sava and later print the response code
+type LoggingWriter struct {
+	http.ResponseWriter // embedded writer type
+	responseCode        int
+}
+
+func (lw *LoggingWriter) WriteHeader(h int) {
+	lw.responseCode = h
+	lw.ResponseWriter.WriteHeader(h)
+}
+
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-
-		inner.ServeHTTP(w, r)
+		lw := &LoggingWriter{w, 0}
+		inner.ServeHTTP(lw, r)
 
 		log.Printf(
-			"%s\t%s\t%s\t%s",
+			"%d\t%s\t%s\t%s\t%s",
+			lw.responseCode,
 			r.Method,
 			r.RequestURI,
 			name,
