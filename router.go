9e4ae1557316e21f544336ade765092edc41936e
Yuval Rimar <yuval.rimar@servicenow.com>
2018-07-24 17:49:52 +0300

    newlines remove

--- router.go
+++ router.go
@@ -7,20 +7,14 @@ import (
 )
 
 func NewRouter() *mux.Router {
-
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
 		var handler http.Handler
-
 		handler = route.HandlerFunc
 		handler = Logger(handler, route.Name)
-
 		router.Methods(route.Method).Path(route.Pattern).Name(route.Name).Handler(handler)
-
 	}
-
 	staticHandler := Logger(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))), "static")
 	router.PathPrefix("/static/").Handler(staticHandler)
-
 	return router
 }
