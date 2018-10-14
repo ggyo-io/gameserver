fba30d078ae979641279147df441695d8c86bdbb
Yuval Rimar <yuval.rimar@servicenow.com>
2018-10-14 14:09:59 +0300

    get rid of error.go

--- handlers.go
+++ handlers.go
@@ -87,9 +87,6 @@ func MakeMove(w http.ResponseWriter, r *http.Request) {
 	// If we didn't find it, 404
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusNotFound)
-	if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusNotFound, Text: "Not Found"}); err != nil {
-		panic(err)
-	}
 
 }
 
