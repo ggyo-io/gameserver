b7124178bd5014723d15fac0e106350b166e9d21
Alexander Indenbaum <alexander.indenbaum@gmail.com>
2018-10-25 21:06:48 +0300

    clean up

--- hub.go
+++ hub.go
@@ -12,9 +12,6 @@ type Hub struct {
 	// Registered clients.
 	clients map[*Client]bool
 
-	// Inbound messages from the clients.
-	broadcast chan *Message
-
 	// Register requests from the clients.
 	register chan *Client
 
