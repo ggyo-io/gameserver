13e4baf5c463c600bafea7ed6d4ac0783a47bf78
Yuval Rimar <yuval.rimar@gmail.com>
2019-03-19 01:22:31 +0200

    go fmt mainly

--- leela.go
+++ leela.go
@@ -3,22 +3,22 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "log"
-    "net/http"
-    "os"
-    "sort"
-    "path/filepath"
-    "strings"
-    "time"
-    "strconv"
-    "net/url"
-    "io/ioutil"
-    "errors"
-    "compress/gzip"
-    "encoding/json"
-    "crypto/rand"
+	"compress/gzip"
+	"crypto/rand"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var networksDir = "networks"
@@ -29,184 +29,184 @@ var HOSTNAME = "http://testserver.lczero.org"
 var curNetId uint = 0
 
 func getExtraParams() map[string]string {
-    randToken := -17
-    randBytes := make([]byte, 2)
-    _, err := rand.Reader.Read(randBytes)
-    if err == nil {
-        randToken = int(randBytes[0]) << 8 | int(randBytes[1])
-    }
-    return map[string]string{
-        "user":     "iwontupload",
-        "password": "hunter2",
-        "version":  "22",
-        "token":    strconv.Itoa(randToken),
-    }
+	randToken := -17
+	randBytes := make([]byte, 2)
+	_, err := rand.Reader.Read(randBytes)
+	if err == nil {
+		randToken = int(randBytes[0])<<8 | int(randBytes[1])
+	}
+	return map[string]string{
+		"user":     "iwontupload",
+		"password": "hunter2",
+		"version":  "22",
+		"token":    strconv.Itoa(randToken),
+	}
 }
 
 func getNetwork(sha string) (string, bool, error) {
-    // Sha already exists?
-    path := filepath.Join(networksDir, sha)
-    if stat, err := os.Stat(path); err == nil {
-        if stat.Size() != 0 {
-            return path, false, nil
-        }
-    }
-    os.MkdirAll(networksDir, os.ModePerm)
+	// Sha already exists?
+	path := filepath.Join(networksDir, sha)
+	if stat, err := os.Stat(path); err == nil {
+		if stat.Size() != 0 {
+			return path, false, nil
+		}
+	}
+	os.MkdirAll(networksDir, os.ModePerm)
 
-    fmt.Printf("Downloading network...\n")
-    // Otherwise, let's download it
-    err := DownloadNetwork(httpClient, HOSTNAME, path, sha)
-    if err != nil {
-        return "", false, err
-    }
-    return path, true, nil
+	fmt.Printf("Downloading network...\n")
+	// Otherwise, let's download it
+	err := DownloadNetwork(httpClient, HOSTNAME, path, sha)
+	if err != nil {
+		return "", false, err
+	}
+	return path, true, nil
 }
 
 func readNetworkSha() string {
-    files, err := ioutil.ReadDir(networksDir)
-    if (err != nil) {
-        log.Printf("Can not read dir %v, error %v\n", networksDir, err)
-        return ""
-    }
-    sort.Slice(files, func(i,j int) bool {
-        return files[i].ModTime().Unix() < files[j].ModTime().Unix()
-    })
-    if (len(files) > 0) {
-       sha := files[0].Name()
-       log.Printf("found network sha %v\n", sha)
-       return sha
-    }
-    log.Printf("dir %v is empty\n", networksDir)
-    return ""
+	files, err := ioutil.ReadDir(networksDir)
+	if err != nil {
+		log.Printf("Can not read dir %v, error %v\n", networksDir, err)
+		return ""
+	}
+	sort.Slice(files, func(i, j int) bool {
+		return files[i].ModTime().Unix() < files[j].ModTime().Unix()
+	})
+	if len(files) > 0 {
+		sha := files[0].Name()
+		log.Printf("found network sha %v\n", sha)
+		return sha
+	}
+	log.Printf("dir %v is empty\n", networksDir)
+	return ""
 }
 
 func updateNetwork() (bool, string) {
-    nextGame, err := NextGame(httpClient, HOSTNAME, getExtraParams())
-    log.Println(nextGame, err)
-    if err != nil {
-        log.Printf("NextGame error %v\n", err)
-        return false, ""
-    }
+	nextGame, err := NextGame(httpClient, HOSTNAME, getExtraParams())
+	log.Println(nextGame, err)
+	if err != nil {
+		log.Printf("NextGame error %v\n", err)
+		return false, ""
+	}
 
-    if nextGame.Type == "train" {
-        _, newNet, err := getNetwork(nextGame.Sha)
-        if err != nil {
-            log.Printf("getNetwork error %v\n", err)
-            return false, ""
-        }
-        curNetId = nextGame.NetworkId
-        return newNet, nextGame.Sha
-    }
-    return false, ""
+	if nextGame.Type == "train" {
+		_, newNet, err := getNetwork(nextGame.Sha)
+		if err != nil {
+			log.Printf("getNetwork error %v\n", err)
+			return false, ""
+		}
+		curNetId = nextGame.NetworkId
+		return newNet, nextGame.Sha
+	}
+	return false, ""
 }
 
 func launchLc0(p *UciEngine, l UciLauncher, sha string) {
-    path := filepath.Join(networksDir, sha)
-    weights := fmt.Sprintf("--weights=%s", path)
-    args := make([]string, 1)
-    args[0] = weights
-    p.launch(l.name(), args, "50", l.moveRequest())
+	path := filepath.Join(networksDir, sha)
+	weights := fmt.Sprintf("--weights=%s", path)
+	args := make([]string, 1)
+	args[0] = weights
+	p.launch(l.name(), args, "50", l.moveRequest())
 }
 
 func leelaStart(l UciLauncher) {
-    httpClient = &http.Client{}
+	httpClient = &http.Client{}
 
-    go func() {
-        var p *UciEngine = nil
+	go func() {
+		var p *UciEngine = nil
 
-        sha := readNetworkSha()
-        if sha != "" {
-            p = &UciEngine{}
-            launchLc0(p, l, sha)
-        } else {
-            log.Printf("Can not find last local lc0 weights network\n")
-        }
+		sha := readNetworkSha()
+		if sha != "" {
+			p = &UciEngine{}
+			launchLc0(p, l, sha)
+		} else {
+			log.Printf("Can not find last local lc0 weights network\n")
+		}
 
-        for {
-            new_net, net_sha := updateNetwork()
-            log.Printf("updateNetwork: new %v sha '%v'\n", new_net, net_sha)
-            if (new_net || p == nil) && net_sha != "" {
-                if p != nil {
-                    mr := MoveRequest{moves: "", bestMove: nil}
-                    l.moveRequest() <- mr
-                    p.Input.Close()
-                }
-                p = &UciEngine{}
-                launchLc0(p, l, net_sha)
+		for {
+			new_net, net_sha := updateNetwork()
+			log.Printf("updateNetwork: new %v sha '%v'\n", new_net, net_sha)
+			if (new_net || p == nil) && net_sha != "" {
+				if p != nil {
+					mr := MoveRequest{moves: "", bestMove: nil}
+					l.moveRequest() <- mr
+					p.Input.Close()
+				}
+				p = &UciEngine{}
+				launchLc0(p, l, net_sha)
 
-                //defer *p.Input.Close()
-            }
-            time.Sleep(12000 * time.Second)
-        }
-    }()
+				//defer *p.Input.Close()
+			}
+			time.Sleep(12000 * time.Second)
+		}
+	}()
 }
 
 type NextGameResponse struct {
-    Type         string
-    TrainingId   uint
-    NetworkId    uint
-    Sha          string
-    CandidateSha string
-    Params       string
-    Flip         bool
-    MatchGameId  uint
+	Type         string
+	TrainingId   uint
+	NetworkId    uint
+	Sha          string
+	CandidateSha string
+	Params       string
+	Flip         bool
+	MatchGameId  uint
 }
 
 func postParams(httpClient *http.Client, uri string, data map[string]string, target interface{}) error {
-    var encoded string
-    if data != nil {
-        values := url.Values{}
-        for key, val := range data {
-            values.Set(key, val)
-        }
-        encoded = values.Encode()
-    }
-    r, err := httpClient.Post(uri, "application/x-www-form-urlencoded", strings.NewReader(encoded))
-    if err != nil {
-        return err
-    }
-    defer r.Body.Close()
-    b, _ := ioutil.ReadAll(r.Body)
-    if target != nil {
-        err = json.Unmarshal(b, target)
-        if err != nil {
-            log.Printf("Bad JSON from %s -- %s\n", uri, string(b))
-        }
-    }
-    return err
+	var encoded string
+	if data != nil {
+		values := url.Values{}
+		for key, val := range data {
+			values.Set(key, val)
+		}
+		encoded = values.Encode()
+	}
+	r, err := httpClient.Post(uri, "application/x-www-form-urlencoded", strings.NewReader(encoded))
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	b, _ := ioutil.ReadAll(r.Body)
+	if target != nil {
+		err = json.Unmarshal(b, target)
+		if err != nil {
+			log.Printf("Bad JSON from %s -- %s\n", uri, string(b))
+		}
+	}
+	return err
 }
 
 func NextGame(httpClient *http.Client, hostname string, params map[string]string) (NextGameResponse, error) {
-    resp := NextGameResponse{}
-    err := postParams(httpClient, hostname+"/next_game", params, &resp)
+	resp := NextGameResponse{}
+	err := postParams(httpClient, hostname+"/next_game", params, &resp)
 
-    if len(resp.Sha) == 0 {
-        return resp, errors.New("Server gave back empty SHA")
-    }
+	if len(resp.Sha) == 0 {
+		return resp, errors.New("Server gave back empty SHA")
+	}
 
-    return resp, err
+	return resp, err
 }
 
 func DownloadNetwork(httpClient *http.Client, hostname string, networkPath string, sha string) error {
-    uri := hostname + fmt.Sprintf("/get_network?sha=%s", sha)
-    r, err := httpClient.Get(uri)
-    defer r.Body.Close()
-    if err != nil {
-        return err
-    }
+	uri := hostname + fmt.Sprintf("/get_network?sha=%s", sha)
+	r, err := httpClient.Get(uri)
+	defer r.Body.Close()
+	if err != nil {
+		return err
+	}
 
-    out, err := os.Create(networkPath)
-    defer out.Close()
-    if err != nil {
-        return err
-    }
+	out, err := os.Create(networkPath)
+	defer out.Close()
+	if err != nil {
+		return err
+	}
 
-    // Copy while decompressing
-    zr, err := gzip.NewReader(r.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Copy while decompressing
+	zr, err := gzip.NewReader(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    _, err = io.Copy(out, zr)
-    return err
+	_, err = io.Copy(out, zr)
+	return err
 }
--- uciplayer.go
+++ uciplayer.go
@@ -1,81 +1,80 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "errors"
+	"encoding/json"
+	"errors"
+	"log"
 )
 
 type UCIPlayer struct {
-    *Player
+	*Player
 
-    bestMove    chan string
+	bestMove chan string
 }
 
 func (c *UCIPlayer) makeMove() (*Message, error) {
-    bm, ok := <-c.bestMove
-    if !ok {
-         log.Printf("uciPlayer '%s' in makeMove(), closed besMove channel'\n", c.user)
-         return nil, errors.New("bestMove channel closed");
-    }
+	bm, ok := <-c.bestMove
+	if !ok {
+		log.Printf("uciPlayer '%s' in makeMove(), closed besMove channel'\n", c.user)
+		return nil, errors.New("bestMove channel closed")
+	}
 
-    log.Printf("uciPlayer '%s' in makeMove() got bestMove '%s'\n", c.user, bm)
-    msg := Message{Cmd: "move", Params: bm}
+	log.Printf("uciPlayer '%s' in makeMove() got bestMove '%s'\n", c.user, bm)
+	msg := Message{Cmd: "move", Params: bm}
 
-    return &msg, nil;
+	return &msg, nil
 }
 
 func (c *UCIPlayer) writePump() {
-    for {
-        select {
-        case mb, ok := <-c.send:
-            log.Printf("uciplayer '%s' got message from send channel: '%s'\n", c.user, string(mb))
-            if !ok {
-                log.Fatal("read send channel error")
-                return
-            }
-            var message Message
-            if err := json.Unmarshal(mb, &message); err != nil {
-                log.Printf("uciplayer '%s' ERROR: Unsupported message format '%s'\n", c.user, string(mb))
-                return
-            }
+	for {
+		select {
+		case mb, ok := <-c.send:
+			log.Printf("uciplayer '%s' got message from send channel: '%s'\n", c.user, string(mb))
+			if !ok {
+				log.Fatal("read send channel error")
+				return
+			}
+			var message Message
+			if err := json.Unmarshal(mb, &message); err != nil {
+				log.Printf("uciplayer '%s' ERROR: Unsupported message format '%s'\n", c.user, string(mb))
+				return
+			}
 
-            switch message.Cmd {
-            case "move":
-                moves   := ""
-                chess := c.gameState.chess
-                for _, move := range chess.Moves() {
-                    moves += " " + move.String()
-                }
-                log.Printf("uciplayer '%s' moves '%s'\n", c.user, moves)
-                mr := MoveRequest{moves: moves, bestMove: c.bestMove}
-                c.hub.robots[c.user].moveRequest() <- mr
+			switch message.Cmd {
+			case "move":
+				moves := ""
+				chess := c.gameState.chess
+				for _, move := range chess.Moves() {
+					moves += " " + move.String()
+				}
+				log.Printf("uciplayer '%s' moves '%s'\n", c.user, moves)
+				mr := MoveRequest{moves: moves, bestMove: c.bestMove}
+				c.hub.robots[c.user].moveRequest() <- mr
 
-            case "outcome":
-                log.Printf("uciplayer '%s' outcome '%s', return from writePump(), signal readPump to exit\n", c.user, message.Params)
-                close(c.bestMove)
-                return
+			case "outcome":
+				log.Printf("uciplayer '%s' outcome '%s', return from writePump(), signal readPump to exit\n", c.user, message.Params)
+				close(c.bestMove)
+				return
 
-            case "disconnect":
-                log.Printf("uciplayer '%s' got 'disconnect', return from writePump(), signal readPump to exit\n", c.user, message.Params)
-                close(c.bestMove)
-                return
+			case "disconnect":
+				log.Printf("uciplayer '%s' got 'disconnect' (%s), return from writePump(), signal readPump to exit\n", c.user, message.Params)
+				close(c.bestMove)
+				return
 
-            case "offer":
-                log.Printf("uciplayer '%s' ignore offer '%s'\n", c.user, message.Params)
+			case "offer":
+				log.Printf("uciplayer '%s' ignore offer '%s'\n", c.user, message.Params)
 
-
-            default:
-                log.Printf("uciplayer '%s' got Unknown command '%s'\n", c.user, string(mb))
-            }
-        }
-    }
+			default:
+				log.Printf("uciplayer '%s' got Unknown command '%s'\n", c.user, string(mb))
+			}
+		}
+	}
 }
 
 func NewUCIPlayer(hub *Hub, user string) *UCIPlayer {
-    player := &Player{hub: hub, user: user, send: make(chan []byte, 256), match: make(chan *GameState)}
-    client := &UCIPlayer{Player: player, bestMove: make(chan string)}
-    client.PlayerI = client
+	player := &Player{hub: hub, user: user, send: make(chan []byte, 256), match: make(chan *GameState)}
+	client := &UCIPlayer{Player: player, bestMove: make(chan string)}
+	client.PlayerI = client
 
-    return client
+	return client
 }
