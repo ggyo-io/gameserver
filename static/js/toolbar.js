db8fc5fd732e3d6ad6a9a12658acdf741d701110
Alexander Indenbaum <alexander.indenbaum@gmail.com>
2019-12-12 20:29:18 +0200

    more toolbar + jquery ui for tooltips

--- static/js/toolbar.js
+++ static/js/toolbar.js
@@ -4,26 +4,55 @@
     window.ToolBar = window.ToolBar || function(cfg) {
         var widget = {};
 
-        var id = 'toolbar-' + createId();
-        var logoutId = 'logout-' + createId();
-        var formId = 'form-' + createId();
+        var id = 'toolbar-' + createId(),
+            logoutId = 'logout-' + createId(),
+            formId = 'form-' + createId(),
+            nameId = 'name-' + createId(),
+            menuId = 'menu-' + createId(),
+            buttonClass = "button-" + createId();
+
+        var styles = [
+            createCssRule('#' + id + '  {display: flex; align-items: center;}'),
+            createCssRule('.' + buttonClass +
+                ':hover {cursor: pointer; background-color: deepskyblue;}')
+        ];
 
         widget.id = function() { return id; };
 
         widget.html = function() {
-            return '<div id="' + id + '">' +
-                '<img src="img/menu.svg" style="float: left;"/>' +
-                '<div style="float: right;"><img src="img/register-black.svg"/>&#8239;<span>Hello, Vasya!</span>&nbsp;&nbsp;<img id="' + logoutId + '" src="img/gear.svg"/></div>' +
-                '<form id="' + formId + '" action="/logout" method="GET"></form>' +
-                '</div>';
+            var html = '<div id="' + id + '">' +
+                '<img id="' + menuId + '" class="' + buttonClass + '" src="img/menu.svg" title="Menu"/>';
+            if (UserName !== '') {
+
+                html += '<span id="' + nameId + '" title="Hello, ' + UserName + '!" >' + UserName + '</span>';
+                html += '<img id="' + logoutId + '" class="' + buttonClass + '" src="img/gear.svg" title="Sign out"/>';
+                html += '<form id="' + formId + '" action="/logout" method="GET"/>';
+
+            } else {
+                //TODO: implement anon/login
+
+            }
+            html += '</div>';
+            return html;
         };
 
         widget.resize = function(top, left, width, height) {
             $('#' + id).css({ top: top, left: left, position: 'absolute', width: width, height: height, 'font-size': height });
-            $('#' + id + ' img').css({ width: height, height: height });
+            $('#' + id + ' img ').attr('width', height);
+            $('#' + id + ' img ').attr('height', height);
+            $('#' + logoutId).css({ left: width - height, position: 'absolute' });
+            if (UserName !== '') {
+                $('#' + nameId).css({ left: (width - height - (height >> 2) - $('#' + nameId).outerWidth(true)), position: 'absolute' });
+            }
         };
 
         widget.events = function() {
+            // jquery ui tooltips
+            $(function() {
+                $(document).tooltip();
+            });
+
+            // Sign out action
             $('#' + logoutId).click(function(e) {
                 console.log('toolbar logout click: ' + e);
                 $('#' + formId).submit();
@@ -32,8 +61,6 @@
 
         return widget;
 
-    }; // end of 
-
-
+    }; // end of ToolBar
 
 })(); // end anonymous wrapper
