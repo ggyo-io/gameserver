a1b2ac419581d5a5765f3b0b8f07cd1b679916c5
mike.zonen <mike.zonen@servicenow.com>
2020-04-29 15:39:24 +0300

    Add template for React with routing and state managment Add websocket

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.scss';
-import Routing from './Routing'
+import Routing from './Routing';
 
 ReactDOM.render(<Routing />, document.querySelector("#root"));
 
