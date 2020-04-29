5e64f71c4e7486c600aee9d5da47c558448fe4e7
mike.zonen <mike.zonen@servicenow.com>
2020-04-29 14:10:58 +0300

    Add template for React with routing and state managment Add websocket

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,6 +10,8 @@ const Home = () => {
         <a href="https://github.com/ctrlplusb/easy-peasy" target="_blank">Easy-Peasy</a>
         <div>UI framework</div>
         <a href="https://react-bootstrap.github.io/getting-started/introduction" target="_blank">React Bootstarp 4</a>
+        <div>Websocket</div>
+        <a href="https://github.com/mehmetkose/react-websocket" target="_blank">react-websocket</a>
         <div>
             <h2>Modal for exaple</h2>
             <Example/>
