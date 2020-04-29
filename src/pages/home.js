e9bf09b5a01f3fd90990e485981b03e40cb8bfa0
mike.zonen <mike.zonen@servicenow.com>
2020-04-29 14:05:55 +0300

    Add template for React with routing and state managment Add example modal

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import {Button} from "react-bootstrap";
+import {Example} from "../components/modal";
+
 const Home = () => {
     return <h2>
         <div>About routing you can read here</div>
@@ -7,6 +10,10 @@ const Home = () => {
         <a href="https://github.com/ctrlplusb/easy-peasy" target="_blank">Easy-Peasy</a>
         <div>UI framework</div>
         <a href="https://react-bootstrap.github.io/getting-started/introduction" target="_blank">React Bootstarp 4</a>
+        <div>
+            <h2>Modal for exaple</h2>
+            <Example/>
+        </div>
     </h2>;
 }
 export {Home};
