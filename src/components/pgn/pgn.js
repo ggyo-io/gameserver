4b8d620f0009468734d1a14248418dae85a42742
rimar <yuval.rimar@gmail.com>
2020-05-08 01:48:49 +0300

    pagination pgn

--- src/components/pgn/pgn.js
+++ src/components/pgn/pgn.js
@@ -1,19 +1,22 @@
 import React from "react";
 import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
 import ToggleButton from "react-bootstrap/ToggleButton";
+import {Pagination} from "react-bootstrap";
 
 export function PGN(props) {
 
     const moves = ['1. e2e4', '1. rf1', '2. d2d4',
         '2. e2e4', '3. rf1', '3. d2d4', '4. d2d4','4. e2e4',].map((move) => (
-        <ToggleButton value={move} variant="outline-secondary" >{move}</ToggleButton>
+        <Pagination.Item key={move} variant="outline-secondary" >{move}</Pagination.Item>
     ))
 
     return (
-        <ToggleButtonGroup name="PGN">
-            <ToggleButton value="back" variant="outline-secondary" >←</ToggleButton>
+        <Pagination name="PGN">
+            <Pagination.First />
+            <Pagination.Prev />
             {moves}
-            <ToggleButton value="forward" variant="outline-secondary" >→</ToggleButton>
-        </ToggleButtonGroup>
+            <Pagination.Next />
+            <Pagination.Last />
+        </Pagination>
     )
 }
