a0798a5f3ab87321d2471feba78c7aeaf8a5ad48
rimar <yuval.rimar@gmail.com>
2020-05-10 15:58:10 +0300

    add controller and chat

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -2,15 +2,18 @@ import Chessboard from "chessboardjsx";
 import React from "react";
 import './board.scss';
 import {Player} from "../player/player";
-import {Moves} from "../moves/moves";
-const MaxBoardSize = 470;
+import {Controller} from "../controller/controller";
+import {PGN} from "../pgn/pgn";
+import {Chat} from "../chat/chat";
+
+const MaxBoardSize = 600;
 const ratio = .6;
 const calculate = (value) => {
     return Math.ceil(value * ratio);
 }
 const getSizes = (props) => {
     const {width, height} = props;
-    const boardSize = height < width ? Math.min(calculate(height), MaxBoardSize): Math.min(calculate(width), MaxBoardSize)
+    const boardSize = height < width ? Math.min(calculate(height), MaxBoardSize) : Math.min(calculate(width), MaxBoardSize)
     return {
         size: boardSize,
         styleWidth: {width: `${boardSize}px`},
@@ -20,25 +23,27 @@ const getSizes = (props) => {
 
 
 export const Board = (props) => {
-   const {position} = props;
-   const {size, styleWidth, styleHeight} = getSizes(props);
-   return <div className="board-content d-flex flex-row justify-content-center">
-        <div className="d-flex flex-column">
-            <div className="mb-2 text-danger align-self-center">Board Width: {size} x {size}</div>
-            <div style={styleWidth} className="align-self-center">
-                <div className="mb-2">
-                    <Player/>
-                </div>
-                <div className="mb-2 p-0 rounded-sm">
-                    <Chessboard id="board" position={position} calcWidth={() => size}/>
-                </div>
-                <div className="mb-2">
-                    <Player/>
+    const {position} = props;
+    const {size, styleWidth, styleHeight} = getSizes(props);
+    return (
+        <div className="board-content d-flex flex-row justify-content-center">
+            <div className="container justify-content-center">
+                <div className="row justify-content-between">
+                    <Chat className="col-3"/>
+                    <div className="col-9 d-flex flex-column justify-content-between pl-3">
+                        <div className="mb-2 text-danger align-self-center">Board Width: {size} x {size}</div>
+                        <PGN/>
+                        <div className="d-inline-flex p-3">
+                            <Chessboard id="board" position={position} calcWidth={() => size}/>
+                            <div className="d-flex flex-column justify-content-between pl-3">
+                                <Player/>
+                                <Controller/>
+                                <Player/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div className="ml-2 align-self-center overflow-auto pr-3 mt-4" style={styleHeight}>
-            <Moves/>
-        </div>
-   </div>
+    )
 }
--- src/components/pgn/pgn.js
+++ src/components/pgn/pgn.js
@@ -1,22 +1,34 @@
 import React from "react";
-import {Pagination} from "react-bootstrap";
 import "./pgn.scss"
 
 export function PGN(props) {
 
-    const mvstrs = ['1. e2e4', '1. rf1', '2. d2d4',
-        '2. e2e4', '3. rf1', '3. d2d4', '4. d2d4','4. e2e4',];
-    const moves = mvstrs.map((move, index) => (
-        <Pagination.Item className="pgn-item" key={move} active={index==mvstrs.length-1?true:false}>{move}</Pagination.Item>
-    ))
+    const style = {
+        // color: '#af8a69'
+        color: '#eddab9'
+    };
+    const mvstrs = ['1.e4', '1.Rf1', '2.d4', '2.e4', '3.Rf1', '3.d4', '4.d4','4.e4'];
+    const moves = mvstrs.map((move, index) => {
+        let active = '';
+        if (index === mvstrs.length - 1)
+            active = ' active';
+
+        return (
+            <li className={"pgn-item page-item" + active}><a style={style}  className="page-link" role="button" href="#">{move}</a></li>
+        )
+    })
 
     return (
-        <Pagination name="PGN">
-            <Pagination.First />
-            <Pagination.Prev />
+        <ul className="pagination" name="PGN">
+            <li className="page-item"><a style={style} className="page-link" role="button" href="#"><span
+                aria-hidden="true">«</span><span className="sr-only">First</span></a></li>
+            <li className="page-item"><a style={style} className="page-link" role="button" href="#"><span
+                aria-hidden="true">‹</span><span className="sr-only">Previous</span></a></li>
             {moves}
-            <Pagination.Next />
-            <Pagination.Last />
-        </Pagination>
+            <li className="page-item"><a style={style} className="page-link" role="button" href="#"><span
+                aria-hidden="true">›</span><span className="sr-only">Next</span></a></li>
+            <li className="page-item"><a style={style} className="page-link" role="button" href="#"><span
+                aria-hidden="true">»</span><span className="sr-only">Last</span></a></li>
+        </ul>
     )
 }
