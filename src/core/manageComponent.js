bd45bb0ec3e0a523ac1a43500133130d99725b04
mike.zonen <mike.zonen@servicenow.com>
2020-05-16 02:08:39 +0300

    Create global Actions Handler

--- src/core/manageComponent.js
+++ src/core/manageComponent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { createStore, Provider, connect } from 'unistore/full/react';
 import devtools from 'unistore/devtools';
-
+let actionHandlersGlobal = {};
 
 export default function menageComponent({...args}) {
     const {initialState, view, actionHandlers, properties} = args;
+    actionHandlersGlobal = Object.assign({}, actionHandlersGlobal, actionHandlers);
     let store = process.env.NODE_ENV === 'production' ?  createStore(initialState) : devtools(createStore(initialState));
     const defaults = {
         mapping: () =>  Object.assign({}, store, store.getState()),
@@ -12,14 +13,14 @@ export default function menageComponent({...args}) {
             dispatch: (type, payload) => {
                 const effect = {type: type, payload: payload};
                 const coeffects = Object.assign({}, {effect: effect}, store, store.getState());
-                return actionHandlers[type]? actionHandlers[type](coeffects): '';
+                return actionHandlersGlobal[type]? actionHandlersGlobal[type](coeffects): '';
             },
             click: (event) => {
                 event.persist();
-              const {_targetInst: {memoizedProps}} = event;
-              const effect = {type: "GAME_BUTTON#CLICKED", payload: memoizedProps || {}, event: event};
-              const coeffects = Object.assign({}, {effect: effect}, store, store.getState());
-              return actionHandlers["GAME_BUTTON#CLICKED"]? actionHandlers["GAME_BUTTON#CLICKED"](coeffects): null;
+                const {_targetInst: {memoizedProps}} = event;
+                const effect = {type: "GAME_BUTTON#CLICKED", payload: memoizedProps || {}, event: event};
+                const coeffects = Object.assign({}, {effect: effect}, store, store.getState());
+                return actionHandlers["GAME_BUTTON#CLICKED"]? actionHandlers["GAME_BUTTON#CLICKED"](coeffects): null;
             }
         })
     }
