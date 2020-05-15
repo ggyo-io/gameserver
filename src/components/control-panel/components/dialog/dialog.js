49f1e2cb942c50a211290874d26e2cfe397069a4
mike.zonen <mike.zonen@servicenow.com>
2020-05-16 01:13:11 +0300

    Implement state management, helpers, mapping, dispatches, click actions wrapper for component

--- src/components/control-panel/components/dialog/dialog.js
+++ src/components/control-panel/components/dialog/dialog.js
@@ -1,23 +1,40 @@
 import React from "react";
 import {Button, Card, ButtonGroup} from "react-bootstrap";
+import createComponent from 'core/manageComponent';
+import {actionHandlers} from "./actionHandlers";
 
-export const Dialog = (props) => (
-    <Card>
+
+const view = (props) => {
+console.info(props)
+const {currentLabel, click} = props;
+    return <Card>
         <Card.Body>
-            <div className="d-flex flex-column alert alert-secondary">
-                <h6 className="align-self-center mb-3">Accept Draw?</h6>
-                <div className="d-inline-flex justify-content-around">
-                    <Button size="sm" variant="primary">Yes</Button>
-                    <Button size="sm" variant="secondary">No</Button>
+            {currentLabel ?
+                <div className="d-flex flex-column alert alert-secondary">
+                    <h6 className="align-self-center mb-3">{currentLabel}</h6>
+                    <div className="d-inline-flex justify-content-around">
+                        <Button size="sm" onClick={click} variant="primary" name="yes">Yes</Button>
+                        <Button size="sm" onClick={click} variant="secondary" name="no">No</Button>
+                    </div>
                 </div>
-            </div>
+                : null
+            }
             <div className="d-flex flex-column">
                 <ButtonGroup size="sm">
-                    <Button>Resign</Button>
-                    <Button>Draw</Button>
-                    <Button>Abort</Button>
+                    <Button name="dialog_resign" onClick={click} >Resign</Button>
+                    <Button name="dialog_draw" onClick={click}>Draw</Button>
+                    <Button name="dialog_abort" onClick={click}>Abort</Button>
                 </ButtonGroup>
             </div>
         </Card.Body>
     </Card>
-)
+}
+
+export const Dialog = () =>  createComponent({
+    view,
+    actionHandlers,
+    initialState: {
+        currentLabel: null,
+        currentTime: 'time'
+    }
+});
