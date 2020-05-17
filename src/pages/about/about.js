a8bd962ae1a081a46a4e0cb1e7008e35fa032614
mike.zonen <mike.zonen@servicenow.com>
2020-05-17 20:46:00 +0300

    add in About testing chess-board

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,10 +1,12 @@
 import React from "react";
+import App from '../../components/new-chess-ground/chess'
 const About = () => {
     return <h2>
-        <div>About routing you can read here</div>
-        <a href="https://reacttraining.com/react-router/web/guides/quick-start" rel="noopener noreferrer" target="_blank">React Router</a>
-        <div>State manager Easy-Peasy</div>
-        <a href="https://github.com/ctrlplusb/easy-peasy" rel="noopener noreferrer" target="_blank">Easy-Peasy</a>
+        {/*<div>About routing you can read here</div>*/}
+        {/*<a href="https://reacttraining.com/react-router/web/guides/quick-start" rel="noopener noreferrer" target="_blank">React Router</a>*/}
+        {/*<div>State manager Easy-Peasy</div>*/}
+        {/*<a href="https://github.com/ctrlplusb/easy-peasy" rel="noopener noreferrer" target="_blank">Easy-Peasy</a>*/}
+        <App/>
     </h2>;
 }
 export {About};
