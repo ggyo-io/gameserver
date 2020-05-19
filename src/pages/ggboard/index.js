dd29aa9fb27c4d1d4ec2958d012c4e7c9989fed5
Alexander Indenbaum <alexander.indenbaum@gmail.com>
2020-05-19 18:34:30 +0300

    Add missing files

---
+++ src/pages/ggboard/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import GGBoard from '../../components/ggboard'
+const GGBoardPage = () => {
+    const style = { maxWidth: "50%"};
+    return <GGBoard style={style}/>;
+}
+export {GGBoardPage};
---
+++ src/pages/gground/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ChessGGround from '../../components/chessgground'
+const GGroundPage = () => {
+    return  <ChessGGround/>;
+}
+export {GGroundPage};
