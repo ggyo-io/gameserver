b5d137fd45b3671182830bd06e18d1aa1d6c6fd6
Alexander Indenbaum <alexander.indenbaum@gmail.com>
2020-05-23 21:23:57 +0300

    fix history

--- src/pages/analysisboard/analysisboard.js
+++ src/pages/analysisboard/analysisboard.js
@@ -3,13 +3,28 @@ import {Board} from "../../components/board/board";
 import {AnalysisPanel} from "../../components/analysis-panel/analysisPanel"
 import {useLocation} from "react-router-dom";
 import {Analysis} from "../../components/board/modes/analysis";
+import {useStoreActions} from 'easy-peasy'
+import Chess from 'chess.js'
+
+const game = new Chess()
 
 export const Analysisboard = () => {
-    const location = useLocation();
-    let pgn = ""
-    if (location.state && location.state.game) {
-        pgn = location.state.game.pgn
-    }
+        const location = useLocation();
+        const update = useStoreActions(actions => actions.game.update)
+        let pgn = ""
+        if (location.state && location.state.game) {
+                pgn = location.state.game.pgn
+                if (game.load_pgn(pgn, { sloppy: true })) {
+                        update({
+                                position: 'start',
+                                history: game.history({ verbose: true }),
+                                browseIndex: 0,
+                                pieceSquare: ''
+                        })
+                }
+        }
+
+
     return <Board
         boardId="analysisboard"
         Mode={<Analysis pgn={pgn}/>}
