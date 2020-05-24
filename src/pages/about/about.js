4b4fd06790fa8800fc8b8faf48f8829aa60698d8
rimar <yuval.rimar@gmail.com>
2020-05-24 23:28:46 +0300

    fix about

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,11 +1,11 @@
 import React from "react";
+
 const About = () => {
     return <h2>
         <div>About routing you can read here</div>
         <a href="https://reacttraining.com/react-router/web/guides/quick-start" rel="noopener noreferrer" target="_blank">React Router</a>
         <div>State manager Easy-Peasy</div>
         <a href="https://github.com/ctrlplusb/easy-peasy" rel="noopener noreferrer" target="_blank">Easy-Peasy</a>
-        <App/>
     </h2>;
 }
 export {About};
