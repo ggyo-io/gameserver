c9cf820598f746d6c7d67523a5d54a7706c254bd
rimar <yuval.rimar@gmail.com>
2020-05-28 02:12:39 +0300

    black/white selected color

--- src/components/board/modes/helpers.js
+++ src/components/board/modes/helpers.js
@@ -1,4 +1,5 @@
-const selectedSquare = 'rgba(248,220,100,0.9)';
+const selectedSquareWhite = 'rgba(248,220,100,0.9)';
+const selectedSquareBlack = 'rgba(201,167,60,0.88)';
 const possibleMove = {
     background:
         'radial-gradient(circle, gray 15%, transparent 25%)',
@@ -16,14 +17,29 @@ export const calcPosition = (history, browseIndex, game) => {
     return game.fen()
 }
 
+function isBlack(square) {
+    if  (!square ||  !square.length || square.length !== 2)
+        return false
+
+    const x = square.charCodeAt(0)
+    const y = square.charCodeAt(1);
+    return (x + y) % 2 === 0
+}
+
 export const pieceSquareStyling = (square) => {
     return {
         ...(square && {
             [square]: {
-                backgroundColor: selectedSquare
+                backgroundColor: color
             }
         })
     }
+}
+
+function selectedSquareColor(square) {
+    console.log("selectedSquareColor: " + square)
+    const color = isBlack(square) ? selectedSquareBlack : selectedSquareWhite
+    return color
 }
 
 export const lastMoveSquareStyling = (history, browseIndex) => {
@@ -33,12 +49,12 @@ export const lastMoveSquareStyling = (history, browseIndex) => {
     return {
         ...(browseIndex && {
             [sourceSquare]: {
-                backgroundColor: selectedSquare
+                backgroundColor: selectedSquareColor(sourceSquare)
             }
         }),
         ...(browseIndex && {
             [targetSquare]: {
-                backgroundColor: selectedSquare
+                backgroundColor: selectedSquareColor(targetSquare)
             }
         })
     }
@@ -56,7 +72,7 @@ export const possibleMovesSquareStyling = (square, game) => {
     if (moves.length === 0) return {}
 
     return {
-        ...{ [square]: { backgroundColor: selectedSquare } },
+        ...{[square]: {backgroundColor: selectedSquareColor(square)}},
         ...moves.map(x => x.to).reduce(
             (a, c) => {
                 return {
