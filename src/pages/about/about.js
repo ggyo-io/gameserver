84b8ba1712e072c9ed85384480ab08ead941bf5e
Alexander Indenbaum <alexander.indenbaum@gmail.com>
2020-07-08 14:49:34 +0300

    Add contact us email link

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 const About = () => {
-    return <><h1>Gg Yo!</h1>
+    return <><h1>GG Yo!</h1>
            <p>
                <strong>ggyo.io</strong> is an online chess platform. You can play anonymously. However certain features, like game history, rating, and more, are available for registered users only.
            </p>
+           <h1>Contact us</h1>
+           <p>
+               You could <a href="mailto:ggyobot@gmail.com">drop us an email</a> if have any question or would like to share your thoughts.
+           </p>
+           <h1>Glossary</h1>
+
            <h2><strong>GG</strong></h2>
            <p>
                <strong>"GG"</strong> stands for "Good Game". In multiplayer competitive games, GG is used as a mark of sportsmanship and an acknowledgment that you had fun while battling against your opponents. For many players, it's standard practice to type GG into the chat at the end of every match.
@@ -15,7 +21,7 @@ const About = () => {
            </p>
            <h2><strong>Stockfish</strong></h2>
            <p>
-               <strong>Stockfish</strong> is a strong <a href="https://stockfishchess.org/">open source</a> chess engine.
+               <strong>Stockfish</strong> is a strong traditional <a href="https://stockfishchess.org/">open source</a> chess engine.
            </p>
            </>
 }
